881caf9021244a67ac047800092926caf1a82cb8
tranhuongk <huongtv.uet@gmail.com>
2020-03-29 16:23:11 +0700

    vertices R-L calculate

--- stream/vertices/augment.js
+++ stream/vertices/augment.js
@@ -1,9 +1,9 @@
 const _ = require('lodash');
 var through = require('through2'),
-    polyline = require('@mapbox/polyline'),
-    project = require('../../lib/project'),
-    analyze = require('../../lib/analyze'),
-    interpolate = require('../../lib/interpolate');
+  polyline = require('@mapbox/polyline'),
+  project = require('../../lib/project'),
+  analyze = require('../../lib/analyze'),
+  interpolate = require('../../lib/interpolate');
 
 // polyline precision
 var PRECISION = 6;
@@ -12,28 +12,28 @@ var PRECISION = 6;
   this stream performs all the interpolation math for a road segment and pushes
   downstream rows to be inserted in the 'street_address' table.
 **/
-function streamFactory(db, done){
+function streamFactory(db, done) {
 
   // create a new stream
-  return through.obj( function( data, enc, next ){
+  return through.obj(function (data, enc, next) {
 
     // decode polyline
-    var coordinates = project.dedupe( polyline.toGeoJSON( data.street.line, PRECISION ).coordinates );
+    var coordinates = project.dedupe(polyline.toGeoJSON(data.street.line, PRECISION).coordinates);
 
     // store an array of housenumbers and their distance along the linestring
     var distances = [];
 
     // process all house number entries in batch
-    data.addresses.forEach( function( address ){
+    data.addresses.forEach(function (address) {
 
       // get variables from db row
-      var point = [ parseFloat( address.proj_lon ), parseFloat( address.proj_lat ) ];
-      var housenumber = parseFloat( address.housenumber );
-      var parity = String( address.parity );
+      var point = [parseFloat(address.proj_lon), parseFloat(address.proj_lat)];
+      var housenumber = parseFloat(address.housenumber);
+      var parity = String(address.parity);
 
       // compute the distance along the linestring to the projected point
-      var proj = project.pointOnLine( coordinates, point );
-      var dist = project.lineDistance( project.sliceLineAtProjection( coordinates, proj ) );
+      var proj = project.pointOnLine(coordinates, point);
+      var dist = project.lineDistance(project.sliceLineAtProjection(coordinates, proj));
 
       // add projection data to distances array
       distances.push({ housenumber: housenumber, dist: dist, parity: parity });
@@ -44,7 +44,7 @@ function streamFactory(db, done){
     // this is important because now the distances and coordinates
     // arrays will run from the start of the street to the end.
     // in case of distance ties, sort on housenumber
-    distances = _.orderBy(distances, ['dist', 'housenumber'], [ 'asc', 'desc']);
+    distances = _.orderBy(distances, ['dist', 'housenumber'], ['asc', 'desc']);
 
     // ----
 
@@ -69,21 +69,21 @@ function streamFactory(db, done){
     };
 
     // iterate distances to enumerate odd/even on L/R
-    distances.forEach( function( cur ){
-      if( cur.housenumber && ( cur.parity === 'R' || cur.parity === 'L' ) ){
-        var isEven = parseInt( cur.housenumber, 10 ) %2;
-        if( isEven ){ ord[cur.parity].even++; }
+    distances.forEach(function (cur) {
+      if (cur.housenumber && (cur.parity === 'R' || cur.parity === 'L')) {
+        var isEven = parseInt(cur.housenumber, 10) % 2;
+        if (isEven) { ord[cur.parity].even++; }
         else { ord[cur.parity].odd++; }
         ord[cur.parity].total++;
       }
     });
 
     // zigzag schemes
-    var zz1 = ( ord.R.odd === ord.R.total && ord.L.even === ord.L.total ),
-        zz2 = ( ord.L.odd === ord.L.total && ord.R.even === ord.R.total );
+    var zz1 = (ord.R.odd === ord.R.total && ord.L.even === ord.L.total),
+      zz2 = (ord.L.odd === ord.L.total && ord.R.even === ord.R.total);
 
     // assign correct scheme to street
-    data.street.scheme = ( zz1 || zz2 ) ? 'zigzag' : 'updown';
+    data.street.scheme = (zz1 || zz2) ? 'zigzag' : 'updown';
 
     // ----
 
@@ -93,43 +93,44 @@ function streamFactory(db, done){
     // insert each point on linestring in table
     // note: this allows us to ignore the linestring and simply linearly
     // interpolation between matched values at query time.
-    coordinates.forEach( function( vertex, i ){
+    coordinates.forEach(function (vertex, i) {
 
       // not a line, just a single point;
-      if( 0 === i ){ return; }
+      if (0 === i) { return; }
 
       // distance along line to this vertex
-      var edge = coordinates.slice(i-1, i+1);
-      vertexDistance += project.lineDistance( edge );
+      var edge = coordinates.slice(i - 1, i + 1);
+      vertexDistance += project.lineDistance(edge);
 
       // projected fractional housenumber(s)
       // note: there may be one or two values produced, depending on the scheme.
-      var housenumbers = [];
+      var housenumbers_R = [];
+      var housenumbers_L = [];
 
       // console.log( data.street.scheme );
 
       // zigzag interpolation
       // (one vertex interpolation produced)
-      if( data.street.scheme === 'zigzag' ){
-        housenumbers.push( interpolate( distances, vertexDistance ) );
-      }
+      // if( data.street.scheme === 'zigzag' ){
+      //   housenumbers.push( interpolate( distances, vertexDistance ) );
+      // }
       // updown interpolation
       // (two vertex interpolations produced)
-      else {
-        // left side
-        housenumbers.push( interpolate( distances.filter( function( d ){
-          return d.parity === 'L';
-        }), vertexDistance ) );
+      // else {
+      // left side
+      housenumbers_L.push(interpolate(distances.filter(function (d) {
+        return d.parity === 'L';
+      }), vertexDistance));
 
-        // right side
-        housenumbers.push( interpolate( distances.filter( function( d ){
-          return d.parity === 'R';
-        }), vertexDistance ) );
-      }
+      // right side
+      housenumbers_R.push(interpolate(distances.filter(function (d) {
+        return d.parity === 'R';
+      }), vertexDistance));
+      // }
 
       // insert point values in db
-      housenumbers.forEach( function( num ){
-        if( !num ){ return; } // skip null interpolations
+      housenumbers_L.forEach(function (num) {
+        if (!num) { return; } // skip null interpolations
         this.push({
           $id: data.street.id,
           $source: 'VERTEX',
@@ -137,7 +138,22 @@ function streamFactory(db, done){
           $housenumber: num.toFixed(3),
           $lon: undefined,
           $lat: undefined,
-          $parity: undefined,
+          $parity: "L",
+          $proj_lon: vertex[0],
+          $proj_lat: vertex[1]
+        });
+      }, this);
+
+      housenumbers_R.forEach(function (num) {
+        if (!num) { return; } // skip null interpolations
+        this.push({
+          $id: data.street.id,
+          $source: 'VERTEX',
+          $source_id: undefined,
+          $housenumber: num.toFixed(3),
+          $lon: undefined,
+          $lat: undefined,
+          $parity: "R",
           $proj_lon: vertex[0],
           $proj_lat: vertex[1]
         });
